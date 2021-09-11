fc3a2d7a782a579471f60910d3b6480c16064504
orun-sunny <sunny.mukut@gmail.com>
2021-09-11 23:31:19 +0600

    practice

---
+++ js/es6.js
@@ -0,0 +1,20 @@
+// const isEven = x => x % 2 == 0;
+// // const addThree = 
+
+// // filter
+
+// const cheap = products.filter(product => product.price <= 5000);
+
+// const numbers = [42, 45];
+// const x = numbers[0];
+// const y = number[1];
+
+// const [x, y] = [42, 45];
+// // console.log(x, y);
+
+function boxify(num1, num2) {
+    const nums = [num1, num2];
+    return nums;
+}
+const [first, second] = [90, 34]
+console.log(boxify(90, 34));
---
+++ js/fetch.js
@@ -0,0 +1,25 @@
+// const studentObj = JSON.parse(studentJSON)
+
+// const keys = Object.keys(student);
+
+
+// const numbers = [10, 20, 15, 18, 144];
+// numbers.forEach(num => console.log(num));
+// numbers.map(num => num * 2);
+
+// const num1 = 50;
+// console.log(num1);
+// const numStr = num1 + '';
+// console.log(numStr);
+
+// const isActive = true;
+// const showUser = () => console.log('display user');
+// const hideUser = () => console.log('hideUser');
+
+let person = null;
+
+console.log(person ? person : "person is null");
+
+const obj = { a: 1 };
+
+console.log(Object.keys(obj).length === 0);
